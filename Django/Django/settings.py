22d01ab0fcc515555b33aabef2a62187cc1d4c07
Connor Carr-Laughlin <cjclmemphis@gmail.com>
2022-10-06 00:07:29 -0500

    Profile/Auth Pages - Resolves #19, #45, #43, #42, #41

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -135,7 +135,6 @@ USE_TZ = True
 STATICFILES_DIRS = [
     BASE_DIR / "static",
     BASE_DIR / "media",
-
 ]
 
 STATIC_URL = '/static/'
@@ -157,3 +156,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
 }
+
+BASE_URL = "http://127.0.0.1:8000"
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760 # 10 * 1024 * 1024 (10 MB)
--- Django/SocialSite/forms.py
+++ Django/SocialSite/forms.py
@@ -7,16 +7,18 @@ from .models import Account
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text="Required. Add a valid email address.")
+    first_name = forms.CharField(max_length=255, help_text="Required. Add a first name.")
+    last_name = forms.CharField(max_length=255, help_text="Required. Add a last name.")
 
     class Meta:
         model = Account
-        fields = ('email', 'username', 'password1', 'password2')
+        fields = ('email', 'username', 'password1', 'password2', 'first_name', 'last_name')
 
     def clean_email(self):
         email = self.cleaned_data['email'].lower()
         try:
             account = Account.objects.get(email=email)
-        except Exception as e:
+        except Account.DoesNotExist:
             return email
         raise forms.ValidationError(f"Email {email} is already in use.")
 
@@ -24,6 +26,58 @@ class RegistrationForm(UserCreationForm):
         username = self.cleaned_data['username']
         try:
             account = Account.objects.get(username=username)
-        except Exception as e:
+        except Account.DoesNotExist:
             return username
         raise forms.ValidationError(f"Username {username} is already in use.")
+
+
+class AccountAuthenticationForm(forms.ModelForm):
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+
+    class Meta:
+        model = Account
+        fields = ("email", "password")
+
+    def clean(self):
+        if self.is_valid():
+            email = self.cleaned_data['email']
+            password = self.cleaned_data['password']
+            if not authenticate(email=email, password=password):
+                raise forms.ValidationError("Invalid Login")
+
+
+class AccountUpdateForm(forms.ModelForm):
+    bio = forms.CharField(label="Bio", required=False, widget=forms.Textarea)
+
+    class Meta:
+        model = Account
+        fields = ('username', 'email', 'profile_image', 'first_name', 'last_name', 'bio', 'hide_email')
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        try:
+            account = Account.objects.exclude(pk=self.instance.pk).get(email=email)
+        except Account.DoesNotExist:
+            return email
+        raise forms.ValidationError(f"Email {email} is already in use.")
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            account = Account.objects.exclude(pk=self.instance.pk).get(username=username)
+        except Account.DoesNotExist:
+            return username
+        raise forms.ValidationError(f"Username {username} is already in use.")
+
+    def save(self, commit=True):
+        account = super(AccountUpdateForm, self).save(commit=False)
+        account.username = self.cleaned_data['username']
+        account.email = self.cleaned_data['email']
+        account.profile_image = self.cleaned_data['profile_image']
+        account.hide_email = self.cleaned_data['hide_email']
+        account.first_name = self.cleaned_data['first_name']
+        account.last_name = self.cleaned_data['last_name']
+        account.bio = self.cleaned_data['bio']
+        if commit:
+            account.save()
+        return account
